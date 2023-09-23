c134bc2fd467ca8fce61f1e96a5a9cb81dc61b1e
Dhruva Shaw <me@dhruvashaw.me>
2023-09-23 18:48:51 +0530

    setup function done

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -2,11 +2,11 @@
 #include <QTRSensors.h>
 
 int EEP = GPIO_NUM_19;
-int in1 = GPIO_NUM_15;
+int in1 = GPIO_NUM_5;
 int in2 = GPIO_NUM_17;
 int in3 = GPIO_NUM_16;
 int in4 = GPIO_NUM_4;
-int VLT = GPIO_NUM_18;
+int ULT = GPIO_NUM_18;
 
 int P;
 int I;
@@ -25,8 +25,6 @@ QTRSensors qtr;
 const uint8_t SensorCount = 8;
 uint16_t sensorValues[SensorCount];
 
-void forward_movement(int speedA, int speedB);
-void PID_control();
 
 void setup() {
   // put your setup code here, to run once:
@@ -38,16 +36,19 @@ void setup() {
   pinMode(in3, OUTPUT);
   pinMode(in4, OUTPUT);
   pinMode(EEP, OUTPUT);
-  pinMode(VLT, INPUT);
-
-  digitalWrite(EEP, HIGH);
+  pinMode(ULT, INPUT_PULLUP);
 
   digitalWrite(in1, LOW);
   digitalWrite(in2, LOW);
   digitalWrite(in3, LOW);
   digitalWrite(in4, LOW);
 
+  digitalWrite(EEP, LOW);
+
   pinMode(button, INPUT_PULLUP);
+  pinMode(buzzer, OUTPUT);
+  pinMode(BUILTIN_LED, OUTPUT);
+
   digitalWrite(BUILTIN_LED, LOW);
   digitalWrite(buzzer, LOW);
 
@@ -59,75 +60,32 @@ void setup() {
     digitalWrite(BUILTIN_LED, LOW);
     digitalWrite(buzzer, LOW);
   }
-  digitalWrite(BUILTIN_LED, LOW);
+
+  digitalWrite(BUILTIN_LED, HIGH);
   digitalWrite(buzzer, LOW);
-  
   //10 seconds
   for (uint16_t i = 0; i < 400; i++)
   {
     qtr.calibrate();
   }
-
-  while(digitalRead(button) == HIGH){}
+  digitalWrite(BUILTIN_LED, LOW);
+  while(digitalRead(button) == HIGH){
+    delay(100);
+    digitalWrite(BUILTIN_LED, HIGH);
+    digitalWrite(buzzer, HIGH);
+    delay(100);
+    digitalWrite(BUILTIN_LED, LOW);
+    digitalWrite(buzzer, LOW);
+  }
   digitalWrite(buzzer, HIGH);
   delay(100);
   digitalWrite(buzzer, LOW);
+  delay(100);
+  digitalWrite(EEP, HIGH);
   delay(1000);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  PID_control();
+ 
 }
 
-void PID_control() {
-  uint16_t positionLine = qtr.readLineBlack(sensorValues);
-  int error = 3500 - positionLine;
-
-  P = error;
-  I = error + I;
-  D = error - lastError;
-  lastError = error; 
-
-  int motorSpeedChange = P*Kp + I*Ki + D*Kd;
-
-  int motorSpeedA = 100 + motorSpeedChange;
-  int motorSpeedB = 100 - motorSpeedChange;
-
-  if (motorSpeedA > 125) {
-    motorSpeedA = 125;
-  }
-  if (motorSpeedB > 125) {
-    motorSpeedB = 125;
-  }
-  if (motorSpeedA < -75) {
-    motorSpeedA = -75;
-  }
-  if (motorSpeedB < -75) {
-    motorSpeedB = -75;
-  }
-  forward_movement(motorSpeedA, motorSpeedB);
-}
-
-void forward_movement(int speedA, int speedB) {
-  if (speedA < 0) {
-    speedA = 0 - speedA;
-    digitalWrite(in2, HIGH);
-    digitalWrite(in1, LOW);
-  }
-  else {
-    digitalWrite(in2, LOW);
-    digitalWrite(in1, LOW);
-  }
-  if (speedB < 0) {
-    speedB = 0 - speedB;
-    digitalWrite(in4, HIGH);
-    digitalWrite(in3, LOW);
-  }
-  else {
-    digitalWrite(in4, LOW);
-    digitalWrite(in3, LOW);
-  }
-  analogWrite(in1, speedA);
-  analogWrite(in3, speedB);
-}
